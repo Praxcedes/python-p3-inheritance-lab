2e166dbff27354c60a7b722fc93e67174907fc7e
Praxcedes <praxcedeskabeya@gmail.com>
2025-09-24 22:10:22 +0300

    python-p3-inheritance-lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
-
 from user import User
 
-import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        # teacher starts with some knowledge
+        self.knowledge = [
+            "Python basics",
+            "Object-oriented programming",
+            "Data structures",
+            "Inheritance",
+            "Unit testing"
+        ]
 
     def teach(self):
-        pass
+        # return a random piece of knowledge
+        import random
+        return random.choice(self.knowledge)
